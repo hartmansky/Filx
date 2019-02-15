05d6a6c196940490c1473710a468d536a3d187be
Sky Hartman <47018249+hartmansky@users.noreply.github.com>
2019-02-14 17:51:25 -0800

    flix part2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Sky Hartman on 2/14/19.
+//  Copyright © 2019 SKY HARTMAN. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
